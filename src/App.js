27a4c740c59835b36a407454b90c21409c617485
RomanVaskov <romavaskov@gmail.com>
2021-02-09 13:06:42 +0300

    Created AboutSection component for About Us page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+
+//Import Pages
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <AboutUs />
     </div>
   );
 }
