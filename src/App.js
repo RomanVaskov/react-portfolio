5de73d9387d9a63655c3b6faf59ecf9017ccfadd
RomanVaskov <romavaskov@gmail.com>
2021-02-17 17:37:30 +0300

    Refactored Nav component. Created pages ContactUs and OurWork

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+// Router
+import { Switch, Route } from "react-router-dom";
+
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
 
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+// Import Components
 import Nav from "./components/Nav";
 
 function App() {
@@ -12,7 +19,17 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
