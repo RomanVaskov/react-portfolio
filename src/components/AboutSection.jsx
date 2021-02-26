851f2c54d414ebc667fad90e94235520098a79fe
RomanVaskov <romavaskov@gmail.com>
2021-02-26 18:49:38 +0300

    Add Framer Motion

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { About, Description, Hide, Image } from "../styles";
 
+// Import Framer Motion
+import { motion } from "framer-motion";
+
 //Import Images
 import home1 from "../img/home1.png";
 
@@ -8,23 +11,23 @@ const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div className="title">
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true</h2>
+            <motion.h2>true</motion.h2>
           </Hide>
           <p>
             Contact us for any ideas. We have professionals with amazing skills
           </p>
           <button>Contact Us</button>
-        </div>
+        </motion.div>
       </Description>
       <Image>
         <img src={home1} alt="guy with a camera" />
